c060ce27c55bf1290440a5919650a444ddf48e6e
MH2033 <aakbarimh@gmail.com>
2025-03-15 10:41:37 +0100

    Complete task3

---
+++ task3/lower_to_llvm.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#Lower to LLVM dialect
+mlir-opt ../task2/matmul_transformed.mlir \
+        -lower-affine \
+        --convert-scf-to-cf \
+        --convert-arith-to-llvm \
+        --convert-func-to-llvm \
+        --convert-memref-to-spirv \
+        --finalize-memref-to-llvm \
+        --reconcile-unrealized-casts \
+        -o matmul_lowered.mlir
+
+#Generate LLVM IR from the lowered mlir
+mlir-translate --mlir-to-llvmir matmul_lowered.mlir -o matmul.ll
